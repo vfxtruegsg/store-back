93294aa6de9049ec1c8e0059ee3c47fcbcbcf6d9
Andrew <andrejpopkov565@gmail.com>
2025-04-17 16:19:11 +0300

    added a middleware for checks authenticate

--- src/routers/store.js
+++ src/routers/store.js
@@ -10,11 +10,16 @@ import {
 } from '../controllers/store.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
 import { postCarSchema, updateCarSchema } from '../validation/store.js';
 
 const router = Router();
 
+router.use(authenticate);
+
 router.get('/', ctrlWrapper(getAllCarsController));
+
+// router.get('/my-cars', ctrlWrapper(getAllCarsController));
 
 router.get('/:id', isValidId, ctrlWrapper(getCarByIdController));
 
